d41edd51df2507d440c5b7a84dfcb3c95408747b
Wess <118078373+WezleyRenan@users.noreply.github.com>
2023-06-02 12:40:07 -0300

    Update FilaVaziaException.java

--- src/br/edu/ifs/ED/fila/FilaVaziaException.java
+++ src/br/edu/ifs/ED/fila/FilaVaziaException.java
@@ -1,4 +1,12 @@
 package br.edu.ifs.ED.fila;
 
 public class FilaVaziaException extends Exception {
+	
+	public FilaVaziaException() {
+		super("a fila está vazia");
+	}
+	public FilaVaziaException(String mensagem) {
+		super(mensagem);
+	}
+	
 }
